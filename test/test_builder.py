dea29637b3e2b6a4230317a68c68ea10f368e2ca
Anthony Shaw <anthonyshaw@apache.org>
2016-07-07 16:55:25 +1000

    fixes to tests

--- test/test_builder.py
+++ test/test_builder.py
@@ -6,13 +6,18 @@ import os
 class TestConfluenceBuilder(unittest.TestCase):
     def setUp(self):
         srcdir = os.path.join(os.getcwd(), 'testproj')
-        self.app = Sphinx(srcdir, srcdir, srcdir, srcdir, 'confluence')
+        self.outdir = os.path.join(srcdir, 'out')
+        confdir = srcdir
+        doctreedir = os.path.join(srcdir, 'doctree')
+
+        self.app = Sphinx(srcdir, confdir, self.outdir, doctreedir, 'confluence')
 
     def test_registry(self):
         self.assertTrue('sphinxcontrib.confluencebuilder' in self.app._extensions.keys())
 
     def test_build(self):
         self.app.build()
+        self.assertTrue(os.path.exists(os.path.join(self.outdir, 'test.conf')))
 
 if __name__ == '__main__':
     import sys
---
+++ test/testproj/test.rst
@@ -0,0 +1,8 @@
+HEADING_TEST
+------------
+
+SUBHEADER_TEST
+==============
+
+- List 1
+- List 2
