bc899cb8c6df6db09219128551933afa950f66d3
James Knight <james.d.knight@live.com>
2017-04-11 02:01:28 -0400

    __init__: cleanup
    Cleanup the order of a series of configuration options and their
    comments. This separates configuration options into a "generic" and
    "publishing" category, as well as organizes options by configuration
    names alphabetically.
    
    Some minor comment and import cleanups as well. No functional changes.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/__init__.py
+++ sphinxcontrib/confluencebuilder/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-    sphinxcontrib.confluencebuilder
-    ===============================
+    sphinxcontrib.confluencebuilder.__init__
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Sphinx extension to output Atlassian Confluence wiki files.
 
@@ -9,10 +9,8 @@
     :license: BSD, see LICENSE.txt for details.
 """
 
-from __future__ import (print_function, unicode_literals, absolute_import)
-
-from sphinx.writers.text import STDINDENT
 from .builder import ConfluenceBuilder
+from sphinx.writers.text import STDINDENT
 import argparse
 
 __version__='0.6.0.dev0'
@@ -30,30 +28,34 @@ def main():
 def setup(app):
     app.require_sphinx('1.0')
     app.add_builder(ConfluenceBuilder)
+    """(generic)"""
+    """Filename suffix for generated files."""
     app.add_config_value('confluence_file_suffix', ".conf", False)
-    """This is the file name suffix for reST files"""
-    app.add_config_value('confluence_link_suffix', None, False)
-    """The is the suffix used in internal links. By default, takes the same value as confluence_file_suffix"""
+    """Translation of docname to a filename."""
     app.add_config_value('confluence_file_transform', None, False)
-    """Function to translate a docname to a filename. By default, returns docname + confluence_file_suffix."""
-    app.add_config_value('confluence_link_transform', None, False)
-    """Function to translate a docname to a (partial) URI. By default, returns docname + confluence_file_suffix."""
-    app.add_config_value('confluence_indent', STDINDENT, False)
-    """Publish to a confluence website"""
-    app.add_config_value('confluence_publish', None, False)
-    """Name of the confluence space to publish to (if publishing)"""
-    app.add_config_value('confluence_space_name', None, False)
-    """Name of the page within the confluence space to use as the root (if publishing)"""
-    app.add_config_value('confluence_parent_page', None, False)
-    """Allow purging legacy child pages from a parent page (if publishing)."""
-    app.add_config_value('confluence_purge', None, False)
-    """URL of the Confluence server to publish to"""
-    app.add_config_value('confluence_server_url', None, False)
-    """ Username to login to Confluence API with """
-    app.add_config_value('confluence_server_user', None, False)
-    """ Password to login to Confluence API with """
-    app.add_config_value('confluence_server_pass', None, False)
-    """ File to get page header information from. """
-    app.add_config_value('confluence_header_file', None, False)
-    """ File to get page footer information from. """
+    """File to get page footer information from."""
     app.add_config_value('confluence_footer_file', None, False)
+    """File to get page header information from."""
+    app.add_config_value('confluence_header_file', None, False)
+    """Indent to use for generated documents."""
+    app.add_config_value('confluence_indent', STDINDENT, False)
+    """Link suffix for generated files."""
+    app.add_config_value('confluence_link_suffix', None, False)
+    """Translation of docname to a (partial) URI."""
+    app.add_config_value('confluence_link_transform', None, False)
+
+    """(publishing)"""
+    """Root/parent page's name to publish documents into."""
+    app.add_config_value('confluence_parent_page', None, False)
+    """Enablement of purging legacy child pages from a parent page."""
+    app.add_config_value('confluence_purge', None, False)
+    """Password to login to Confluence API with."""
+    app.add_config_value('confluence_server_pass', None, False)
+    """Username to login to Confluence API with."""
+    app.add_config_value('confluence_server_user', None, False)
+    """URL of the Confluence server to publish to."""
+    app.add_config_value('confluence_server_url', None, False)
+    """Confluence Space to publish to."""
+    app.add_config_value('confluence_space_name', None, False)
+    """Enablement of publishing."""
+    app.add_config_value('confluence_publish', None, False)
