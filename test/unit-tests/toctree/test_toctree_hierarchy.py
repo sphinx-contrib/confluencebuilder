ea9300e9f02d57c361f4f8d615ae4abb88457bbd
James Knight <james.d.knight@live.com>
2018-01-23 01:31:25 -0500

    test: refactor toctree hierarchy unit test
    Refactor the toctree hierarchy test from the deprecated 'publish' unit
    test into 'test_common' unit test (managed via the 'test_main' suite).
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ test/unit-tests/toctree/test_toctree_hierarchy.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2017-2018 by the contributors (see AUTHORS file).
+    :license: BSD, see LICENSE.txt for details.
+"""
+
+from sphinxcontrib.confluencebuilder.state import ConfluenceState
+from sphinxcontrib_confluencebuilder_util import ConfluenceTestUtil as _
+import os
+import unittest
+
+class TestConfluenceToctreeHierarchyMarkup(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        config = _.prepareConfiguration()
+        config['confluence_max_doc_depth'] = 1
+        config['confluence_page_hierarchy'] = True
+        config['master_doc'] = 'toctree'
+
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        dataset = os.path.join(test_dir, 'dataset-hierarchy')
+        self.expected = os.path.join(test_dir, 'expected-hierarchy')
+
+        doc_dir, doctree_dir = _.prepareDirectories('toctree-hierarchy')
+        app = _.prepareSphinx(dataset, doc_dir, doctree_dir, config)
+        app.build(force_all=True)
+
+        self.doc_dir = doc_dir
+        self.app = app
+
+    def test_max_depth(self):
+        _.assertExpectedWithOutput(
+            self, 'toctree', self.expected, self.doc_dir)
+        _.assertExpectedWithOutput(
+            self, 'toctree-doc1', self.expected, self.doc_dir)
+        _.assertExpectedWithOutput(
+            self, 'toctree-doc2', self.expected, self.doc_dir)
+        _.assertExpectedWithOutput(
+            self, 'toctree-doc3', self.expected, self.doc_dir)
+
+    def test_parent_registration(self):
+        root_doc = ConfluenceState.parentDocname('toctree')
+        self.assertIsNone(root_doc, 'root toctree has a parent')
+
+        parent_doc = ConfluenceState.parentDocname('toctree-doc1')
+        self.assertEqual(parent_doc, 'toctree',
+            'toctree-doc1 parent is not root toctree')
+
+        parent_doc = ConfluenceState.parentDocname('toctree-doc2')
+        self.assertEqual(parent_doc, 'toctree',
+            'toctree-doc2 parent is not root toctree')
+
+        parent_doc = ConfluenceState.parentDocname('toctree-doc3')
+        self.assertEqual(parent_doc, 'toctree',
+            'toctree-doc3 parent is not root toctree')
+
+        parent_doc = ConfluenceState.parentDocname('toctree-doc2a')
+        self.assertEqual(parent_doc, 'toctree-doc2',
+            'toctree-doc2a parent is not toctree-doc2')
