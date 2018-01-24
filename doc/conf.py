cc449af3eaea05a0df41b2df1f0492ae18419108
James Knight <james.d.knight@live.com>
2018-01-24 00:32:17 -0500

    doc: theme corrections
    Override select style options in the Read the Docs theme [1] to improve
    markup support documentation. This addresses:
     - Prevents cell content being forcefully wrapped and hidden.
     - Prevents lists inside tables from adding excessive margins.
    
    [1]: https://github.com/rtfd/sphinx_rtd_theme
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- doc/conf.py
+++ doc/conf.py
@@ -102,6 +102,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [ '_static/theme_overrides.css' ],
+    }
 
 # -- Options for HTMLHelp output ------------------------------------------
 
