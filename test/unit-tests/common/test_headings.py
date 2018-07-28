37a912fd3082b652c6fba90eae60859e34478ee2
James Knight <james.d.knight@live.com>
2018-07-28 13:54:22 -0400

    test: remove duplicate headings dataset
    For heading-removing unit tests, both `dataset-headings-default` and
    `dataset-headings-with-title` use the exact same content. With
    adjustments made with `assertExpectedWithOutput`, unit tests can share
    datasets when comparing with expected results. Adjusting the unit test
    take advantage of this.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- test/unit-tests/common/test_headings.py
+++ test/unit-tests/common/test_headings.py
@@ -15,26 +15,25 @@ from pkg_resources import iter_entry_points
 class TestConfluenceCommonHeadings(unittest.TestCase):
     @classmethod
     def setUpClass(self):
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+
         self.config = _.prepareConfiguration()
-        self.test_dir = os.path.dirname(os.path.realpath(__file__))
-        self.expected = os.path.join(self.test_dir, 'expected')
+        self.dataset = os.path.join(test_dir, 'dataset-headings')
+        self.expected = os.path.join(test_dir, 'expected')
 
     def test_headings_default(self):
-        dataset = os.path.join(self.test_dir, 'dataset-headings-default')
-
         doc_dir, doctree_dir = _.prepareDirectories('headings-default')
-        app = _.prepareSphinx(dataset, doc_dir, doctree_dir, self.config)
+        app = _.prepareSphinx(self.dataset, doc_dir, doctree_dir, self.config)
         app.build(force_all=True)
         _.assertExpectedWithOutput(
-            self, 'headings-default', self.expected, doc_dir)
+            self, 'headings-default', self.expected, doc_dir, tpn='headings')
 
     def test_headings_with_title(self):
         config = dict(self.config)
         config['confluence_remove_title'] = False
-        dataset = os.path.join(self.test_dir, 'dataset-headings-with-title')
 
         doc_dir, doctree_dir = _.prepareDirectories('headings-with-title')
-        app = _.prepareSphinx(dataset, doc_dir, doctree_dir, config)
+        app = _.prepareSphinx(self.dataset, doc_dir, doctree_dir, config)
         app.build(force_all=True)
         _.assertExpectedWithOutput(
-            self, 'headings-with-title', self.expected, doc_dir)
+            self, 'headings-with-title', self.expected, doc_dir, tpn='headings')
