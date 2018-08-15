74e624a5662cb5bbb84813046b3eb3a4234f8954
James Knight <james.d.knight@live.com>
2018-08-14 21:07:05 -0400

    test: adding unit tests for manpage
    With the introduction of the manpage role support, adding a respective
    unit test checks.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ test/unit-tests/common/dataset-manpage/contents.rst
@@ -0,0 +1,6 @@
+.. http://www.sphinx-doc.org/en/master/usage/restructuredtext/roles.html?highlight=manpage#role-manpage
+
+manpage
+-------
+
+:manpage:`ls(1)`
---
+++ test/unit-tests/common/test_manpage.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2018 by the contributors (see AUTHORS file).
+    :license: BSD-2-Clause, see LICENSE for details.
+"""
+
+from sphinxcontrib_confluencebuilder_util import ConfluenceTestUtil as _
+import os
+import unittest
+
+class TestConfluenceManpage(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+
+        self.config = _.prepareConfiguration()
+        self.dataset = os.path.join(test_dir, 'dataset-manpage')
+        self.expected = os.path.join(test_dir, 'expected')
+
+    def test_manpage_with_config(self):
+        config = dict(self.config)
+        config['manpages_url'] = 'https://manpages.example.com/{path}'
+
+        doc_dir, doctree_dir = _.prepareDirectories('manpage-conf')
+        app = _.prepareSphinx(self.dataset, doc_dir, doctree_dir, config)
+        app.build(force_all=True)
+        _.assertExpectedWithOutput(
+            self, 'manpage-conf', self.expected, doc_dir, tpn='contents')
+
+    def test_manpage_without_config(self):
+        doc_dir, doctree_dir = _.prepareDirectories('manpage-noconf')
+        app = _.prepareSphinx(self.dataset, doc_dir, doctree_dir, self.config)
+        app.build(force_all=True)
+        _.assertExpectedWithOutput(
+            self, 'manpage-noconf', self.expected, doc_dir, tpn='contents')
