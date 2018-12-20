6f5a44a6f83ebc91b6c8c2f11b1b86492caa9196
jhermann <jh@web.de>
2018-12-20 12:03:09 +0100

    Make '-Dconfluence_publish=...' work
    When passed on the command line, options are strings. Make a few
    common negative strings known to 'confluence_publish'.

--- sphinxcontrib/confluencebuilder/builder.py
+++ sphinxcontrib/confluencebuilder/builder.py
@@ -124,7 +124,8 @@ class ConfluenceBuilder(Builder):
         else:
             self.lang_transform = None
 
-        if self.config.confluence_publish:
+        if self.config.confluence_publish and (self.config.confluence_publish.lower()
+                                               not in ('0', 'false', 'n', 'no', 'off')):
             self.publish = True
             self.publisher.connect()
         else:
