f9ca8d04303c830ec521f3735826c4152e37ac90
Anthony Shaw <anthonyshaw@apache.org>
2019-03-14 14:20:26 +1100

    sphinxcontrib-confluencebuilder 0.10

--- sphinxcontrib/confluencebuilder/__init__.py
+++ sphinxcontrib/confluencebuilder/__init__.py
@@ -10,7 +10,7 @@ from .translator import ConfluenceTranslator
 from sphinx.writers.text import STDINDENT
 import argparse
 
-__version__='0.10.0.dev0'
+__version__='0.10'
 
 def main():
     parser = argparse.ArgumentParser(prog=__name__,
