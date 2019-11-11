b4d2d6b566fac1539250613c50554f55abcb3c2d
Jesse Tan <j.tan@activevideo.com>
2019-11-11 20:16:40 +0100

    Do not fail with NotImplementedError on math_block
    Signed-off-by: Jesse Tan <j.tan@activevideo.com>

--- sphinxcontrib/confluencebuilder/translator.py
+++ sphinxcontrib/confluencebuilder/translator.py
@@ -1300,6 +1300,10 @@ class ConfluenceTranslator(BaseTranslator):
         # unsupported
         raise nodes.SkipNode
 
+    def visit_math_block(self, node):
+        # unsupported
+        raise nodes.SkipNode
+
     # -------------------------
     # sphinx -- production list
     # -------------------------
