085f284643326727d4c5f578b3e11588cc6b0a8c
James Knight <james.d.knight@live.com>
2020-01-01 17:22:19 -0500

    __init__: recategorize configuration options
    Recategorize the configuration options provided by this extension. No
    functional changes made.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/__init__.py
+++ sphinxcontrib/confluencebuilder/__init__.py
@@ -74,76 +74,61 @@ def setup(app):
                 app.registry.get_post_transforms().remove(transform)
                 break
 
-    """(essential)"""
+    # ##########################################################################
+
+    """(configuration - essential)"""
     """Enablement of publishing."""
     app.add_config_value('confluence_publish', None, False)
     """API key/password to login to Confluence API with."""
     app.add_config_value('confluence_server_pass', None, False)
-    """Username to login to Confluence API with."""
-    app.add_config_value('confluence_server_user', None, False)
     """URL of the Confluence instance to publish to."""
     app.add_config_value('confluence_server_url', None, False)
+    """Username to login to Confluence API with."""
+    app.add_config_value('confluence_server_user', None, False)
     """Confluence Space to publish to."""
     app.add_config_value('confluence_space_name', None, False)
 
-    """(generic)"""
-    """Explicitly prevent page notifications on update."""
-    app.add_config_value('confluence_disable_notifications', None, False)
+    """(configuration - generic)"""
+    """Add page and section numbers if doctree has :numbered: option"""
+    app.add_config_value('confluence_add_secnumbers', True, False)
     """File to get page header information from."""
     app.add_config_value('confluence_header_file', None, False)
     """File to get page footer information from."""
     app.add_config_value('confluence_footer_file', None, False)
-    """Enablement of configuring master as space's homepage."""
-    app.add_config_value('confluence_master_homepage', None, False)
     """Enablement of the maximum document depth (before inlining)."""
     app.add_config_value('confluence_max_doc_depth', None, False)
     """Enablement of publishing pages into a hierarchy from a master toctree."""
     app.add_config_value('confluence_page_hierarchy', None, False)
-    """Root/parent page's name to publish documents into."""
-    app.add_config_value('confluence_parent_page', None, False)
     """Show previous/next buttons (bottom, top, both, None)."""
     app.add_config_value('confluence_prev_next_buttons_location', None, False)
+    """Suffix to put after section numbers, before section name"""
+    app.add_config_value('confluence_secnumber_suffix', '. ', False)
+
+    """(configuration - publishing)"""
+    """Request for publish password to come from interactive session."""
+    app.add_config_value('confluence_ask_password', False, False)
+    """Request for publish username to come from interactive session."""
+    app.add_config_value('confluence_ask_user', False, False)
+    """Explicitly prevent auto-generation of titles for titleless documents."""
+    app.add_config_value('confluence_disable_autogen_title', None, False)
+    """Explicitly prevent page notifications on update."""
+    app.add_config_value('confluence_disable_notifications', None, False)
+    """Enablement of configuring master as space's homepage."""
+    app.add_config_value('confluence_master_homepage', None, False)
+    """Root/parent page's name to publish documents into."""
+    app.add_config_value('confluence_parent_page', None, False)
     """Postfix to apply to title of published pages."""
     app.add_config_value('confluence_publish_postfix', None, False)
     """Prefix to apply to published pages."""
     app.add_config_value('confluence_publish_prefix', None, False)
-    """Add page and section numbers if doctree has :numbered: option"""
-    app.add_config_value('confluence_add_secnumbers', True, False)
-    """Suffix to put after section numbers, before section name"""
-    app.add_config_value('confluence_secnumber_suffix', '. ', False)
     """Enablement of purging legacy child pages from a parent page."""
     app.add_config_value('confluence_purge', None, False)
     """Enablement of purging legacy child pages from a master page."""
     app.add_config_value('confluence_purge_from_master', None, False)
+    """Timeout for network-related calls (publishing)."""
+    app.add_config_value('confluence_timeout', None, False)
 
-    """(advanced-configuration - authentication)"""
-    """Authentication passthrough for Confluence REST interaction."""
-    app.add_config_value('confluence_server_auth', None, False)
-    """Cookie(s) to use for Confluence REST interaction."""
-    app.add_config_value('confluence_server_cookies', None, False)
-
-    """(advanced-configuration - processing)"""
-    """Filename suffix for generated files."""
-    app.add_config_value('confluence_file_suffix', ".conf", False)
-    """Translation of docname to a filename."""
-    app.add_config_value('confluence_file_transform', None, False)
-    """Indent to use for generated documents."""
-    app.add_config_value('confluence_indent', STDINDENT, False)
-    """Translation of a raw language to code block macro language."""
-    app.add_config_value('confluence_lang_transform', None, False)
-    """Link suffix for generated files."""
-    app.add_config_value('confluence_link_suffix', None, False)
-    """Translation of docname to a (partial) URI."""
-    app.add_config_value('confluence_link_transform', None, False)
-    """Remove a detected title from generated documents."""
-    app.add_config_value('confluence_remove_title', True, False)
-
-    """(advanced-configuration - publishing)"""
-    """Request for publish username to come from interactive session."""
-    app.add_config_value('confluence_ask_user', False, False)
-    """Request for publish password to come from interactive session."""
-    app.add_config_value('confluence_ask_password', False, False)
-    """File/path to Certificate Authority"""
+    """(configuration - advanced publishing)"""
     """Tri-state asset handling (auto, force push or disable)."""
     app.add_config_value('confluence_asset_override', None, False)
     """File/path to Certificate Authority"""
@@ -152,8 +137,6 @@ def setup(app):
     app.add_config_value('confluence_client_cert', None, False)
     """Password for client certificate to use for publishing"""
     app.add_config_value('confluence_client_cert_pass', None, False)
-    """Explicitly prevent auto-generation of titles for titleless documents."""
-    app.add_config_value('confluence_disable_autogen_title', None, False)
     """Explicitly prevent any Confluence REST API callers."""
     app.add_config_value('confluence_disable_rest', None, False)
     """Disable SSL validation with Confluence server."""
@@ -166,12 +149,28 @@ def setup(app):
     app.add_config_value('confluence_proxy', None, False)
     """Subset of document names to publish"""
     app.add_config_value('confluence_publish_subset', [], False)
-    """Timeout for network-related calls (publishing)."""
-    app.add_config_value('confluence_timeout', None, False)
+    """Authentication passthrough for Confluence REST interaction."""
+    app.add_config_value('confluence_server_auth', None, False)
+    """Cookie(s) to use for Confluence REST interaction."""
+    app.add_config_value('confluence_server_cookies', None, False)
+
+    """(configuration - advanced processing)"""
+    """Filename suffix for generated files."""
+    app.add_config_value('confluence_file_suffix', ".conf", False)
+    """Translation of docname to a filename."""
+    app.add_config_value('confluence_file_transform', None, False)
     """Configuration for named JIRA Servers"""
     app.add_config_value('confluence_jira_servers', {}, True)
+    """Translation of a raw language to code block macro language."""
+    app.add_config_value('confluence_lang_transform', None, False)
+    """Link suffix for generated files."""
+    app.add_config_value('confluence_link_suffix', None, False)
+    """Translation of docname to a (partial) URI."""
+    app.add_config_value('confluence_link_transform', None, False)
+    """Remove a detected title from generated documents."""
+    app.add_config_value('confluence_remove_title', True, False)
 
-    """(advanced - undocumented)"""
+    """(configuration - undocumented)"""
     """Enablement for aggressive descendents search (for purge)."""
     app.add_config_value('confluence_adv_aggressive_search', None, False)
     """Enablement of the children macro for hierarchy mode."""
@@ -184,6 +183,10 @@ def setup(app):
     app.add_config_value('confluence_adv_trace_data', False, False)
     """Do not cap sections to a maximum of six (6) levels."""
     app.add_config_value('confluence_adv_writer_no_section_cap', None, False)
+    """Indent to use for generated documents."""
+    app.add_config_value('confluence_indent', STDINDENT, False)
+
+    # ##########################################################################
 
     """JIRA directives"""
     """Adds the custom nodes needed for JIRA directives"""
