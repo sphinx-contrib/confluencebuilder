33c438304c26d90f460e8d5ecc4cd06fdf78d252
James Knight <james.d.knight@live.com>
2020-01-01 16:26:59 -0500

    doc: bumping copyright year
    Signed-off-by: James Knight <james.d.knight@live.com>

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Sphinx Confluence Builder'
-copyright = '2019. Anthony Shaw and contributors'
+copyright = '2020. Anthony Shaw and contributors'
 author = 'Anthony Shaw and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
