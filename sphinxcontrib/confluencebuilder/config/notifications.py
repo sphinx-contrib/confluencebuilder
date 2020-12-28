888111c07558b510ee938091b19b7f5fcef93cb4
James Knight <james.d.knight@live.com>
2020-12-27 23:02:32 -0500

    config: add a series of deprecated/warning messages
    The following introduces a series of warning messages for unique
    configuration scenarios and configured marked as deprecated. This
    includes:
    
    - Warn when a user provides a file suffix ending with a period (which
       gets replaced by a default value when detected).
    - Always warn when a user configures an environment to disable SSL
       verification.
    - When a custom mime type is provided and is not detected as a valid
       mime type, generate a warning.
    - A series of advanced configuration options are planned to be removed
       in a future release (to reduce maintenance on them). Marking these
       options as deprecated for the upcoming release.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/config/notifications.py
+++ sphinxcontrib/confluencebuilder/config/notifications.py
@@ -5,11 +5,18 @@
 """
 
 from sphinxcontrib.confluencebuilder.logger import ConfluenceLogger
+import mimetypes
 
 """
 dictionary of deprecated configuration entries and associated message
 """
 DEPRECATED_CONFIGS = {
+    'confluence_adv_hierarchy_child_macro':
+        'to be removed in a future version',
+    'confluence_adv_trace_data':
+        'to be removed in a future version',
+    'confluence_adv_writer_no_section_cap':
+        'to be removed in a future version',
     'confluence_publish_subset':
         'use "confluence_publish_allowlist" instead',
 }
@@ -33,6 +40,11 @@ def deprecated(validator):
         if config[key] is not None:
             ConfluenceLogger.warn('%s deprecated; %s' % (key, msg))
 
+    # promote singleconfluence over confluence_max_doc_depth=0
+    if config.confluence_max_doc_depth == 0:
+        ConfluenceLogger.warn('%s with a value of zero is deprecated; '
+            "use the 'singleconfluence' builder instead" % key)
+
 def warnings(validator):
     """
     inform users of any warnings related to a configuration state
@@ -45,4 +57,23 @@ def warnings(validator):
         validator: the configuration validator
     """
 
-    pass
+    config = validator.config
+
+    # check if any user defined mime types are unknown
+    if config.confluence_additional_mime_types is not None:
+        for mime_type in config.confluence_additional_mime_types:
+            if not mimetypes.guess_extension(mime_type):
+                ConfluenceLogger.warn('confluence_additional_mime_types '
+                    'defines an unknown mime type: ' + mime_type)
+
+    # warn when ssl validation is disabled
+    if config.confluence_disable_ssl_validation:
+        ConfluenceLogger.warn('confluence_disable_ssl_validation is set; '
+            'consider using confluence_ca_cert instead')
+
+    # confluence_file_suffix "cannot" end with a dot
+    if (config.confluence_file_suffix and
+            config.confluence_file_suffix.endswith('.')):
+        ConfluenceLogger.warn(
+            'confluence_file_suffix ends with a period; '
+            'a default value will be applied instead')
