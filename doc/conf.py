2bf303ff6a540579cad2a2d906ebc5009dcf5c88
James Knight <james.d.knight@live.com>
2021-09-26 13:19:13 -0400

    doc: sync eol confluence versions
    Confluence versions before 7.1 have reached end-of-life and will not be
    tested in next release cycle system tests. Unlisting the version as
    supported.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ author = 'Sphinx Confluence Builder Contributors'
 version = sphinxcontrib.confluencebuilder.__version__
 release = sphinxcontrib.confluencebuilder.__version__
 
-supported_confluence_ver = '6.13+'
+supported_confluence_ver = '7.1+'
 supported_python_ver = '2.7 or 3.6+'
 supported_sphinx_ver = '1.8 or 3.4+'
 
