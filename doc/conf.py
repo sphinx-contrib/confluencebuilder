e56878fafafe9ecdfbd439036a1b7a25dbfd47f6
James Knight <james.d.knight@live.com>
2021-09-26 11:29:39 -0400

    doc: dropping redirects
    Dropping the usage of the redirect in the Sphinx configurations.
    Redirects will be managed on the ReadTheDocs project itself.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- doc/conf.py
+++ doc/conf.py
@@ -46,9 +46,6 @@ html_static_path = ['_static']
 
 html_additional_pages = {
     'index': 'index.html',
-    # interim redirect of a compatibilities-to-features for external links which
-    # may not have been updated yet
-    'compatibilities': 'compatibilities-redirect.html',
 }
 
 html_sidebars = {
