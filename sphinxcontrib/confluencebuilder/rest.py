ae9cb563a22439496c814879a52f102012ab70b8
James Knight <james.d.knight@live.com>
2021-10-23 10:00:43 -0400

    avoid configuring rest session proxies if no proxy is set
    Adjusting all REST session building to only configure the proxy value if
    a configuration assigns an explicit proxy value. This prevents get/etc.
    errors on older versions of requests which assume a set `http`/`https`
    proxy value is a valid string (where calls fail to handle a `None`
    value).
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/rest.py
+++ sphinxcontrib/confluencebuilder/rest.py
@@ -57,10 +57,11 @@ class Rest:
             'X-Atlassian-Token': NOCHECK,
         })
         session.timeout = config.confluence_timeout
-        session.proxies = {
-            'http': config.confluence_proxy,
-            'https': config.confluence_proxy,
-        }
+        if config.confluence_proxy is not None:
+            session.proxies = {
+                'http': config.confluence_proxy,
+                'https': config.confluence_proxy,
+            }
 
         # add custom header options based off the user's configuration
         if config.confluence_publish_headers:
