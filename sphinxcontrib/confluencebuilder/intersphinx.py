378cbe3f13cec17dd6ab361a56fcf140f76eabc7
James Knight <james.d.knight@live.com>
2021-10-23 21:43:51 -0400

    copyright header bump
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/intersphinx.py
+++ sphinxcontrib/confluencebuilder/intersphinx.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-:copyright: Copyright 2020 Sphinx Confluence Builder Contributors (AUTHORS)
+:copyright: Copyright 2020-2021 Sphinx Confluence Builder Contributors (AUTHORS)
 :copyright: Copyright 2007-2020 by the Sphinx team (sphinx-doc/sphinx#AUTHORS)
 :license: BSD-2-Clause (LICENSE)
 """
