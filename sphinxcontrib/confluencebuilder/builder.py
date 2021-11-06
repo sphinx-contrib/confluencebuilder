b78fbf59282ff17923201a2ddc26aeeec6174f39
James Knight <james.d.knight@live.com>
2021-11-06 18:01:43 -0400

    builder: adjusting purge actions to use a status iterator
    Adjusting both legacy page processing and legacy asset processing to use
    a status iterator (for percentage progress) over a simple dot-per-entity
    progress.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/builder.py
+++ sphinxcontrib/confluencebuilder/builder.py
@@ -596,38 +596,27 @@ class ConfluenceBuilder(Builder):
                 return
 
             if self.legacy_pages:
-                n = len(self.legacy_pages)
-                self.info('removing legacy pages (total: {}) ...'.format(n),
-                    nonl=(not self._verbose))
-
-                for legacy_page_id in self.legacy_pages:
+                for legacy_page_id in status_iterator(
+                        self.legacy_pages, 'removing legacy pages... ',
+                        length=len(self.legacy_pages),
+                        verbosity=self._verbose):
                     self.publisher.removePage(legacy_page_id)
                     # remove any pending assets to remove from the page (as they
                     # are already been removed)
                     self.legacy_assets.pop(legacy_page_id, None)
 
-                    if not self._verbose:
-                        self.info('.', nonl=True)
-
-                if not self._verbose:
-                    self.info(' done\n')
-
             n = 0
             for legacy_asset_info in self.legacy_assets.values():
                 n += len(legacy_asset_info.keys())
             if n > 0:
-                self.info('removing legacy assets (total: {}) ...'.format(n),
-                    nonl=(not self._verbose))
+                for legacy_asset_info in status_iterator(
+                        self.legacy_assets.values(),
+                        'removing legacy assets... ',
+                        length=n,
+                        verbosity=self._verbose):
 
-                for legacy_asset_info in self.legacy_assets.values():
                     for id in legacy_asset_info.keys():
                         self.publisher.removeAttachment(id)
-
-                    if not self._verbose:
-                        self.info('.', nonl=True)
-
-                if not self._verbose:
-                    self.info(' done\n')
 
     def finish(self):
         # restore environment's get_doctree if it was temporarily replaced
