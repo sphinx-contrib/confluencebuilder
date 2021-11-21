240651ed3761b6089346aa070639903811558c44
James Knight <james.d.knight@live.com>
2021-11-20 21:18:43 -0500

    tests-validation: add page generation notice
    Enabling the page generation notice in the validation testing.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -35,6 +35,7 @@ class TestConfluenceValidation(unittest.TestCase):
         cls.config = prepare_conf()
         cls.config['confluence_disable_notifications'] = True
         cls.config['confluence_page_hierarchy'] = True
+        cls.config['confluence_page_generation_notice'] = True
         cls.config['confluence_parent_page'] = None
         cls.config['confluence_prev_next_buttons_location'] = 'both'
         cls.config['confluence_publish'] = True
