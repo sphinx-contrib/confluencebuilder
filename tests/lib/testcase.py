44e009c1ec3d5c7ba3ef4d27de8c7134c9f19a87
James Knight <james.d.knight@live.com>
2022-01-30 18:31:25 -0500

    tests: introduce confluence test case class
    Introduces a new class `ConfluenceTestCase` to provide common
    capabilities across multiple unit tests which prepare/build a Sphinx
    dataset. This new class aims to do two things (in this initial
    implementation):
    
     1) Provide default initialization for common settings/paths. The class
         can default generate a valid initial configuration for unit tests
         to use without needing to explicit create a common/dummy
         configuration. The attribute `config` will be always available for
         a unit test class. In addition, common directories will be
         populated as well. The attributes `assets_dir`, `datasets` and
         `templates_dir` will be available without having each class needing
         to determine the proper pathing for these directories.
    
     2) Provide an easy way to define builder options (and in the future,
         parser options). When preparing a Sphinx setup or invoke a Sphinx
         builder, configuration and build options need to be explicitly set
         in the argument options for these calls. There is a desire to
         minimize the need to explicitly inject configuration options if a
         unit test does not need to override/set defaults. Using the new
         test base's `build`/`prepare` calls, the configuration used can be
         the class instance's prepare configuration if not explicitly set.
    
         There is also a desire to define which builder type (and in the
         future, editor/parser type) using decorators, without needing to
         explicit pass in these options in the build/prepare calls. Using a
         decorator-like style, it should be easier to define tests as we
         introduce newer builders/formats to test/parse against.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/lib/testcase.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+:copyright: Copyright 2022 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
+"""
+
+from functools import wraps
+from tests.lib import build_sphinx
+from tests.lib import prepare_conf
+from tests.lib import prepare_sphinx
+import os
+import unittest
+
+# default builder to test against
+DEFAULT_BUILDER = 'confluence'
+
+
+class ConfluenceTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """
+        confluence test case
+
+        Provides a test case for unit testing which includes helper calls to
+        build/prepare Sphinx environments and helps managing parsing. Unit
+        tests can take advantage of available decorators to help configure
+        their setups.
+
+        For example:
+
+            @setup_builder('confluence')
+            def test_storage_example(self):
+                out_dir = self.build(self.dataset)
+
+                ...
+        """
+
+        super(ConfluenceTestCase, self).__init__(*args, **kwargs)
+
+        self.builder = DEFAULT_BUILDER
+
+    @classmethod
+    def setUpClass(cls):
+        # always prepare a dummy configuration a unit test could use
+        cls.config = prepare_conf()
+
+        # provide a reference to common directories
+        lib_dir = os.path.dirname(os.path.realpath(__file__))
+        tests_dir = os.path.join(lib_dir, os.pardir)
+        unit_tests_dir = os.path.join(tests_dir, 'unit-tests')
+
+        cls.assets_dir = os.path.join(unit_tests_dir, 'assets')
+        cls.datasets = os.path.join(unit_tests_dir, 'datasets')
+        cls.templates_dir = os.path.join(unit_tests_dir, 'templates')
+
+    def build(self, *args, **kwargs):
+        """
+        helper to invoke `build_sphinx` with decorator configured options
+
+        Args:
+            *args: argument values to forward
+            **kwargs: kw-argument values to forward
+        """
+
+        new_kwargs = dict(kwargs)
+        new_kwargs.setdefault('builder', self.builder)
+        new_kwargs.setdefault('config', self.config)
+
+        return build_sphinx(*args, **new_kwargs)
+
+    def prepare(self, *args, **kwargs):
+        """
+        helper to invoke `prepare_sphinx` with decorator configured options
+
+        Args:
+            *args: argument values to forward
+            **kwargs: kw-argument values to forward
+        """
+
+        new_kwargs = dict(kwargs)
+        new_kwargs.setdefault('builder', self.builder)
+        new_kwargs.setdefault('config', self.config)
+
+        return prepare_sphinx(*args, **new_kwargs)
+
+
+def setup_builder(builder):
+    """
+    prepare a confluence unit test for a specific builder configuration
+
+    A utility "decorator" to help setup builder options for a unit test. This
+    avoids the need to explicitly configure a builder options directly in a
+    build/prepare call for a unit test testing against a dataset.
+
+    Args:
+        builder: the builder to use
+    """
+
+    def _decorator(func):
+        @wraps(func)
+        def _wrapper(self, *args, **kwargs):
+            self.builder = builder
+
+            return func(self, *args, **kwargs)
+        return _wrapper
+    return _decorator
