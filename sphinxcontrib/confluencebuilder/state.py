2b9af80056a818243f03d5004d9616b605d8e620
Anton <box4android@gmail.com>
2022-05-25 15:46:03 +0100

    Fixing title trimming for the applying postfix case. Signed-off-by: Anton Mihalkov <box4android@gmail.com>
    Signed-off-by: Anton <box4android@gmail.com>

--- sphinxcontrib/confluencebuilder/state.py
+++ sphinxcontrib/confluencebuilder/state.py
@@ -88,12 +88,13 @@ class ConfluenceState:
 
         if postfix:
             base_tail += postfix
-            try_max += len(postfix)
 
-        if len(title) > try_max:
-            title = title[0:try_max]
-            logger.warn('document title has been trimmed due to '
-                'length: %s' % docname)
+        if len(title) + len(base_tail) > try_max:
+            warning = 'document title has been trimmed due to length: %s' % title
+            if len(base_tail) > 0:
+                warning += '; With postfix: %s' % base_tail
+            logger.warn(warning)
+            title = title[0:try_max - len(base_tail)]
 
         base_title = title
         title += base_tail
