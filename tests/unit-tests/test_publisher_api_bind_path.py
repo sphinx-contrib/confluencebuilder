03ee080356257e82ecc14eede5feba19e9c1f77c
James Knight <james.d.knight@live.com>
2022-05-28 18:02:43 -0400

    tests: validate new api prefix disablement option
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/test_publisher_api_bind_path.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+:copyright: Copyright 2022 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
+"""
+
+from sphinxcontrib.confluencebuilder.publisher import ConfluencePublisher
+from tests.lib import autocleanup_publisher
+from tests.lib import mock_confluence_instance
+from tests.lib import prepare_conf_publisher
+import unittest
+
+
+class TestConfluencePublisherApiBindPath(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.config = prepare_conf_publisher()
+
+        cls.std_space_connect_rsp = {
+            'size': 1,
+            'results': [{
+                'name': 'Mock Space',
+                'type': 'global',
+            }],
+        }
+
+    def test_publisher_api_bind_path_default(self):
+        """validate publisher includes api bind path"""
+        #
+        # Verify that a publisher will perform requests which target the
+        # `/rest/api` endpoint.
+
+        with mock_confluence_instance(self.config) as daemon, \
+                autocleanup_publisher(ConfluencePublisher) as publisher:
+            daemon.register_get_rsp(200, self.std_space_connect_rsp)
+
+            publisher.init(self.config)
+            publisher.connect()
+
+            # connect request
+            connect_req = daemon.pop_get_request()
+            self.assertIsNotNone(connect_req)
+            req_path, _ = connect_req
+            self.assertTrue('/rest/api/' in req_path)
+
+    def test_publisher_api_bind_path_disabled(self):
+        """validate publisher can disable api bind path"""
+        #
+        # Verify that a publisher can perform requests disables the use of
+        # the `/rest/api` endpoint.
+
+        config = self.config.clone()
+        config.confluence_publish_disable_api_prefix = True
+
+        with mock_confluence_instance(config) as daemon, \
+                autocleanup_publisher(ConfluencePublisher) as publisher:
+            daemon.register_get_rsp(200, self.std_space_connect_rsp)
+
+            publisher.init(config)
+            publisher.connect()
+
+            # connect request
+            connect_req = daemon.pop_get_request()
+            self.assertIsNotNone(connect_req)
+            req_path, _ = connect_req
+            self.assertTrue('/rest/api/' not in req_path)
