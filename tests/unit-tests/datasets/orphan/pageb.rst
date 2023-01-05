6e47552ce37fc98cfba2e198d4565e9429479ab0
James Knight <james.d.knight@live.com>
2023-01-05 14:50:23 -0500

    tests: adding checks to verify orphan page options
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/unit-tests/datasets/orphan/pageb.rst
@@ -0,0 +1,6 @@
+:orphan:
+
+pageb
+=====
+
+content
---
+++ tests/unit-tests/test_config_orphan.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+:copyright: Copyright 2023 Sphinx Confluence Builder Contributors (AUTHORS)
+:license: BSD-2-Clause (LICENSE)
+"""
+
+from sphinxcontrib.confluencebuilder.builder import ConfluenceBuilder
+from sphinxcontrib.confluencebuilder.publisher import ConfluencePublisher
+from tests.lib.testcase import ConfluenceTestCase
+from unittest.mock import ANY
+from unittest.mock import call
+from unittest.mock import patch
+import os
+
+
+class TestConfluenceConfigOrphan(ConfluenceTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestConfluenceConfigOrphan, cls).setUpClass()
+
+        cls.dataset = os.path.join(cls.datasets, 'orphan')
+
+        cls.config['confluence_publish'] = True
+        cls.config['confluence_server_url'] = 'https://dummy.example.com/'
+        cls.config['confluence_space_key'] = 'DUMMY'
+
+    def run(self, result=None):
+        with patch.object(ConfluencePublisher, 'connect'), \
+                patch.object(ConfluencePublisher, 'disconnect'), \
+                patch.object(ConfluenceBuilder, 'publish_asset'):
+            super(TestConfluenceConfigOrphan, self).run(result)
+
+    def test_config_orphan_allow_explicit(self):
+        config = dict(self.config)
+        config['confluence_publish_orphan'] = True
+
+        with patch.object(ConfluenceBuilder, 'publish_doc') as mm:
+            self.build(self.dataset, config=config)
+
+        self.assertEqual(mm.call_count, 3)
+        mm.assert_has_calls([
+            call('index', ANY),
+            call('pagea', ANY),
+            call('pageb', ANY),
+        ], any_order=True)
+
+    def test_config_orphan_allow_default(self):
+        with patch.object(ConfluenceBuilder, 'publish_doc') as mm:
+            self.build(self.dataset)
+
+        self.assertEqual(mm.call_count, 3)
+        mm.assert_has_calls([
+            call('index', ANY),
+            call('pagea', ANY),
+            call('pageb', ANY),
+        ], any_order=True)
+
+    def test_config_orphan_deny_explicit(self):
+        config = dict(self.config)
+        config['confluence_publish_orphan'] = False
+
+        with patch.object(ConfluenceBuilder, 'publish_doc') as mm:
+            self.build(self.dataset, config=config)
+
+        self.assertEqual(mm.call_count, 2)
+        mm.assert_has_calls([
+            call('index', ANY),
+            call('pagea', ANY),
+        ], any_order=True)
