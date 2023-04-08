1c10db0c1f60246356e30a7fd1cc023b3a6f9237
James Knight <james.d.knight@live.com>
2023-04-08 13:16:02 -0400

    tests-sample-latex: adding sample set
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/sample-sets/latex/conf.py
@@ -0,0 +1,20 @@
+extensions = [
+    'sphinxcontrib.confluencebuilder',
+    'sphinx.ext.imgmath',
+]
+
+imgmath_font_size = 14
+imgmath_image_format = 'svg'
+imgmath_use_preview = True
+
+# Various Marketplace LaTeX addons:
+#
+# confluence_latex_macro = {
+#    'block-macro': 'mathblock',
+#    'inline-macro': 'mathinline',
+#    'inline-macro-param': 'body',
+# }
+# confluence_latex_macro = 'latex-formatting'
+# confluence_latex_macro = 'mathblock'
+# confluence_latex_macro = 'orah-latex'
+# confluence_latex_macro = None
