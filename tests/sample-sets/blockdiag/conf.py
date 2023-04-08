24621af2ee0b0082c193d3b89a423603914dc409
James Knight <james.d.knight@live.com>
2023-04-08 13:14:38 -0400

    tests-sample-blockdiag: adding sample set
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/sample-sets/blockdiag/conf.py
@@ -0,0 +1,12 @@
+extensions = [
+    'sphinxcontrib.confluencebuilder',
+    # blockdiag extensions
+    'sphinxcontrib.actdiag',
+    'sphinxcontrib.blockdiag',
+    'sphinxcontrib.nwdiag',
+    'sphinxcontrib.packetdiag',
+    'sphinxcontrib.rackdiag',
+    'sphinxcontrib.seqdiag',
+]
+
+blockdiag_image_format = 'svg'
