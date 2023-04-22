1743fc6325b4bda2a7003041164cf8552a62279a
James Knight <james.d.knight@live.com>
2023-04-22 15:55:47 -0400

    tests-sample-sphinx-toolbox: adding sample set
    Signed-off-by: James Knight <james.d.knight@live.com>

---
+++ tests/sample-sets/sphinx-toolbox/conf.py
@@ -0,0 +1,15 @@
+extensions = [
+    'sphinxcontrib.confluencebuilder',
+    # sphinx_toolbox extensions
+    'sphinx_toolbox.assets',
+    'sphinx_toolbox.code',
+    'sphinx_toolbox.collapse',
+    'sphinx_toolbox.flake8',
+    'sphinx_toolbox.formatting',
+    'sphinx_toolbox.issues',
+    'sphinx_toolbox.shields',
+    'sphinx_toolbox.wikipedia',
+]
+
+github_username = 'sphinx-contrib'
+github_repository = 'confluencebuilder'
