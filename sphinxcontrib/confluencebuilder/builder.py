818df91c2e0fd15f2c0e9a68dd4083e94c6e7380
James Knight <james.d.knight@live.com>
2023-07-14 17:55:54 -0400

    code cleanup (noqa on builder format attribute)
    While the `format` attribute does shadow the global `format` call, this
    is a Sphinx-related attribute and out of our control.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/builder.py
+++ sphinxcontrib/confluencebuilder/builder.py
@@ -46,7 +46,7 @@ class ConfluenceBuilder(Builder):
     allow_parallel = True
     default_translator_class = ConfluenceStorageFormatTranslator
     name = 'confluence'
-    format = 'confluence_storage'
+    format = 'confluence_storage'  # noqa: A003
     supported_image_types = ConfluenceSupportedImages()
     supported_linkcode = True
     supported_remote_images = True
