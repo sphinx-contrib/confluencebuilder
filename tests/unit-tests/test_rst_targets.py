e9a5b926126b3f5026479cef2d4dac1e6a8efe17
James Knight <james.d.knight@live.com>
2023-11-19 16:32:37 -0500

    tests: add v2-specific anchor unit test for rst
    When testing reStructuredText's anchor creation, test for both the v1
    and v2 editors, to see expected results.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- tests/unit-tests/test_rst_targets.py
+++ tests/unit-tests/test_rst_targets.py
@@ -4,6 +4,7 @@
 from tests.lib.parse import parse
 from tests.lib.testcase import ConfluenceTestCase
 from tests.lib.testcase import setup_builder
+from tests.lib.testcase import setup_editor
 import os
 
 
@@ -35,3 +36,23 @@ class TestConfluenceRstTargets(ConfluenceTestCase):
 
             trailing_data = anchor_tag.nextSibling.strip()
             self.assertEqual(trailing_data, 'inline target example.')
+
+    @setup_builder('confluence')
+    @setup_editor('v2')
+    def test_storage_rst_targets_v2(self):
+        out_dir = self.build(self.dataset)
+
+        with parse('index', out_dir) as data:
+            # sanity check anchor creation
+            anchor_tag = data.find('ac:structured-macro')
+            self.assertIsNotNone(anchor_tag)
+            self.assertTrue(anchor_tag.has_attr('ac:name'))
+            self.assertEqual(anchor_tag['ac:name'], 'anchor')
+
+            anchor_param = anchor_tag.find('ac:parameter', recursive=False)
+            self.assertIsNotNone(anchor_param)
+            self.assertEqual(anchor_param.text, 'inline-target')
+
+            # before text
+            before_data = anchor_tag.previousSibling.strip()
+            self.assertEqual(before_data, 'An')
