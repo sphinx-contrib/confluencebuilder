b9cefd8df4575022e2d998e0fd4f6c8e28c2f828
James Knight <james.d.knight@live.com>
2024-08-05 12:51:35 -0400

    force sphinx 7.1.x minimum
    Configuring a minimum version of Sphinx to v7.1.x. We only support up to
    five major-minor versions of Sphinx, and with Sphinx v8 released,
    setting a new lowest version. This should prevent any issues from
    arising for versions we no longer actively test on.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- sphinxcontrib/confluencebuilder/__init__.py
+++ sphinxcontrib/confluencebuilder/__init__.py
@@ -55,7 +55,7 @@ def setup(app):
     ConfluenceLogger.initialize()
     cm = app.config_manager_ = ConfigManager(app)
 
-    app.require_sphinx('6.1')
+    app.require_sphinx('7.1')
     app.add_builder(ConfluenceBuilder)
     app.add_builder(ConfluenceReportBuilder)
     app.add_builder(SingleConfluenceBuilder)
