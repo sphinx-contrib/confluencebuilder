4abf8211460f56919c751686e7d089ba5e8967dd
mkonig <koenig@v-i-t.de>
2025-09-26 12:30:43 +0200

    Add unit test for the new option.
    Signed-off-by: mkonig <koenig@v-i-t.de>

--- tests/unit-tests/test_config_checks.py
+++ tests/unit-tests/test_config_checks.py
@@ -435,6 +435,25 @@ class TestConfluenceConfigChecks(unittest.TestCase):
         self.config['confluence_header_file'] = relbase + 'sample-header.tpl'
         self._try_config()
 
+    def test_config_check_confluence_table_width(self):
+        self.config['confluence_table_width'] = 123456
+        self._try_config()
+
+        self.config['confluence_table_width'] = '123456'
+        self._try_config()
+
+        self.config['confluence_table_width'] = 0
+        with self.assertRaises(ConfluenceConfigError):
+            self._try_config()
+
+        self.config['confluence_table_width'] = -123456
+        with self.assertRaises(ConfluenceConfigError):
+            self._try_config()
+
+        self.config['confluence_table_width'] = 'MyPage'
+        with self.assertRaises(ConfluenceConfigError):
+            self._try_config()
+
     def test_config_check_confluence_html_macro(self):
         self.config['confluence_html_macro'] = ''
         self._try_config()
